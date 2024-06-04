7609429703b6958150cbd6bb5bc4ea75e77fc9ef
RRL-dev <reznik.roni@gmail.com>
2024-06-04 14:05:10 +0300

    remove BaseElastic

--- lightchain/connector/__init__.py
+++ lightchain/connector/__init__.py
@@ -1,8 +1,6 @@
-from .elastic import BaseElastic  # noqa: D104
 from .sql import FilesToSQL, SQLDatabase, get_table_info_from_sql_database
 
 __all__: list[str] = [
-    "BaseElastic",
     "FilesToSQL",
     "SQLDatabase",
     "get_table_info_from_sql_database",
