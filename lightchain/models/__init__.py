685a11022df4ac702b81c468efb91adabf9ea41a
RRL-dev <reznik.roni@gmail.com>
2024-06-16 13:53:42 +0300

    refactor: remove causal

--- lightchain/models/__init__.py
+++ lightchain/models/__init__.py
@@ -1,11 +1,11 @@
-from .causal import BaseModelForCausalLM
 from .decoder import Config, ForwardParams, GPT2LMHeadModel, GPT2Model, load_weight
+from .encoder import SentenceMPNet
 
 __all__: list[str] = [
-    "BaseModelForCausalLM",
     "GPT2Model",
     "GPT2LMHeadModel",
     "load_weight",
     "Config",
     "ForwardParams",
+    "SentenceMPNet",
 ]
