550db56d8cb616857c8d5978b4826dc49be61e43
RRL-dev <roni.reznik@gmail.com>
2024-07-02 15:13:33 +0300

    refactor: add ChainedRunnable

--- nltkp/modules/rag/message/synthesizers/summarizer/simple.py
+++ nltkp/modules/rag/message/synthesizers/summarizer/simple.py
@@ -1,34 +1,31 @@
 """The module provides an implementation of the ContentSynthesizer base called SimpleSummarizer.
 
-It returns the provided text as is, truncated to fit within
-a specified maximum context window. This class can be used in applications where
-simple concatenation and truncation of input text are required without further processing.
+It returns the provided text as is, concatenated into a single string.
+This class can be used in applications where simple concatenation of input text chunks is required without processing.
 """
 
 from __future__ import annotations
 
-from nltkp.modules.rag.message.synthesizers import ContentSynthesizer
+from nltkp.factory import ChainedRunnable
 
 
-class SimpleSummarizer(ContentSynthesizer):
+class SimpleSummarizer(ChainedRunnable[list[str], str]):
     """A simple implementation of the ContentSynthesizer that returns the provided text as is."""
 
-    def generate_response(
-        self: SimpleSummarizer,
-        context: list[str],
-    ) -> str:
-        """Return the provided text chunks as a single string, respecting the maximum context window.
+    def __init__(self: SimpleSummarizer) -> None:
+        """Initialize the SimpleSummarizer with optional parameters."""
+        ChainedRunnable.__init__(self=self, func=self.generate_response)
+
+    def generate_response(self: SimpleSummarizer, inputs: list[str]) -> str:
+        """Return the provided text chunks as a single string.
 
         Args:
         ----
-            context (list[str]): A list of text chunks to summarize or return.
-
+            inputs (list[str]): List of text chunks to concatenate.
 
         Returns:
         -------
-            str: The text returned as a single concatenated string.
+            str: The concatenated text returned as a single string.
 
         """
-        # Concatenate text ensuring it does not exceed the max_context_window
-        full_text: str = "\n".join(context)
-        return full_text[: self.max_context_window]
+        return "\n".join(inputs)
