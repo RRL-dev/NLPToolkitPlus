a371f9d4af90dc92901cc8ea47047ad51a7b33d1
RRL-dev <roni.reznik@gmail.com>
2024-07-17 10:47:41 +0300

    chore: init generation module

---
+++ nltkp/modules/generation/__init__.py
@@ -0,0 +1,11 @@
+from .model import (
+    BasePhi3Instruct,
+    input_preprocess,
+    output_postprocess,
+)
+
+__all__: list[str] = [
+    "BasePhi3Instruct",
+    "output_postprocess",
+    "input_preprocess",
+]
