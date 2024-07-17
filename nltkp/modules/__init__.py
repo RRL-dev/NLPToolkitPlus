c974ac40fb30fdd37c51b2d7f8aad08405ee3784
RRL-dev <roni.reznik@gmail.com>
2024-07-17 10:51:51 +0300

    chore: remove sql part

--- nltkp/modules/__init__.py
+++ nltkp/modules/__init__.py
@@ -1,8 +1,6 @@
-from .agent import SqlAgent
 from .reader import FolderTextSplitter, FolderTextSplitterConfig
 
 __all__: list[str] = [
-    "SqlAgent",
     "FolderTextSplitter",
     "FolderTextSplitterConfig",
 ]
