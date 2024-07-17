9d7b6a9537a225694b75e4fa6b590f624829fac1
RRL-dev <roni.reznik@gmail.com>
2024-07-17 10:53:33 +0300

    chore: remove gpt init

--- nltkp/models/__init__.py
+++ nltkp/models/__init__.py
@@ -1,13 +1,5 @@
-from .chat import BaseChatModel
-from .decoder import Config, ForwardParams, GPT2LMHeadModel, GPT2Model, load_weight
 from .sentence import BaseSentenceModel
 
 __all__: list[str] = [
-    "BaseChatModel",
-    "GPT2Model",
-    "GPT2LMHeadModel",
-    "load_weight",
-    "Config",
-    "ForwardParams",
     "BaseSentenceModel",
 ]
