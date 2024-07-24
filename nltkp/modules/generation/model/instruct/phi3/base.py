18f8f99097aa7c84c00c80de2e0c5b83df2868ef
RRL-dev <roni.reznik@gmail.com>
2024-07-24 12:46:11 +0300

    feat: add ChainedRunnable

--- nltkp/modules/generation/model/instruct/phi3/base.py
+++ nltkp/modules/generation/model/instruct/phi3/base.py
@@ -7,19 +7,21 @@ from typing import TYPE_CHECKING, Any
 from torch import Tensor, device, inference_mode
 from transformers.tokenization_utils_base import BatchEncoding
 
+from nltkp.factory.chain.runnable import ChainedRunnable
 from nltkp.models.decoder import Phi3Sample
 from nltkp.models.utils import clean_module_name, move_tensors_to_device
 from nltkp.modules.generation.model.base import BaseGeneration
 from nltkp.modules.generation.model.postprocess import output_postprocess
 from nltkp.modules.generation.model.preprocess import input_preprocess
 from nltkp.modules.generation.params import SamplingParams
+from nltkp.modules.rag.retriever import SearchInput
 from nltkp.utils import LOGGER, set_device
 
 if TYPE_CHECKING:
     from transformers.tokenization_utils_base import BatchEncoding
 
 
-class BasePhi3Instruct(Phi3Sample, BaseGeneration):
+class BasePhi3Instruct(Phi3Sample, BaseGeneration, ChainedRunnable[SearchInput | list[dict[str, str]], list[dict[Any, Any]]]):
     """Base class for generating responses using a pre-trained language model."""
 
     model_params = SamplingParams()
@@ -33,6 +35,8 @@ class BasePhi3Instruct(Phi3Sample, BaseGeneration):
 
         """
         super().__init__(pretrained_model_name=self.model_params.model_name)
+        ChainedRunnable.__init__(self=self, func=self.forward)
+
         self.generation_config.update(**self.model_params.__dict__)
 
         self.model_name: str = clean_module_name(name=self.model_params.model_name)
@@ -69,7 +73,7 @@ class BasePhi3Instruct(Phi3Sample, BaseGeneration):
             LOGGER.info("Generating sequence.")
             self.generate()
 
-            generated_sequence: Tensor = self.model_params.tokens_ids[:, input_info.input_ids.shape[1]:]
+            generated_sequence: Tensor = self.model_params.tokens_ids[:, input_info.input_ids.shape[1] :]
             self.model_params.output_ids = generated_sequence.to(device="cpu")
             self.model_params.prompt_text = input_info.prompt_text
 
